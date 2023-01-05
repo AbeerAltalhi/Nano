15cf3d0cc1da08776c46ece82d8782f4cbebf70a
Abeer Altalhi <abeerabdrabu17@gmail.com>
2023-01-05 12:35:14 +0300

    nano project

---
+++ nano/Camera.swift
@@ -0,0 +1,20 @@
+//
+//  Camera.swift
+//  nano
+//
+//  Created by Abeer on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+struct Camera: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Camera_Previews: PreviewProvider {
+    static var previews: some View {
+        Camera()
+    }
+}
---
+++ nano/Splash.swift
@@ -0,0 +1,20 @@
+//
+//  Splash.swift
+//  nano
+//
+//  Created by Abeer on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Splash_Previews: PreviewProvider {
+    static var previews: some View {
+        Splash()
+    }
+}

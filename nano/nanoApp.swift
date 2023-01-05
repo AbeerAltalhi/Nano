ad02b52a9fedfa691d2477613268b8cef0b7477b
Abeer Altalhi <abeerabdrabu17@gmail.com>
2023-01-05 12:32:18 +0300

    Initial Commit

---
+++ nano/nanoApp.swift
@@ -0,0 +1,20 @@
+//
+//  nanoApp.swift
+//  nano
+//
+//  Created by Abeer on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct nanoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

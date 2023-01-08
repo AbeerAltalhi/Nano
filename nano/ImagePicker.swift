e6995af2af328baf26fba80b9ef8a4993cf718fc
Abeer Altalhi <abeerabdrabu17@gmail.com>
2023-01-08 09:25:46 +0300

    nano

---
+++ nano/ImagePicker.swift
@@ -0,0 +1,49 @@
+//
+//  ImagePicker.swift
+//  nano
+//
+//  Created by Abeer on 13/06/1444 AH.
+//
+
+import SwiftUI
+import UIKit
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+     
+    @Binding var selectedImage: UIImage
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) ->  UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+      //  imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+//    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//        var parent: ImagePicker
+//        init( parent: ImagePicker){
+//            self.parent = parent
+//        }
+//        func imagePickerControllerDidCancel(_ picker: UIImagePickerController, didFinishPickerMediaWithInfo: [UIImagePickerController.InfoKey : Any]) {
+//
+//            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//                parent.selectedImage = image
+//            }
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//    }
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(parent: self)
+//    }
+}
+
---
+++ nano/MainPage.swift
@@ -0,0 +1,49 @@
+//
+//  MainPage.swift
+//  nano
+//
+//  Created by Abeer on 13/06/1444 AH.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    @State var changeProfileImage = false
+    @State var openCameraRoll = false
+    @State var imageSelected = UIImage()
+    var body: some View {
+        
+        VStack {
+            Text("أُنقر هنا ")
+            Button(action:{
+                changeProfileImage = true
+                openCameraRoll = true
+            }, label: {
+                if changeProfileImage{
+                    Image(uiImage: imageSelected)
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                }else{
+                    
+                    Image(systemName: "camera")
+                        .resizable()
+                        .frame(width: 60.0, height: 50.0)
+                      //  .padding()
+                }
+            })
+            VStack{
+                Text("إرشادات : ")
+                Text("- يجب إلتقاط الصوره بإضاءّ جيدة. ")
+                Text("- التأكد من وضوح الصورة عند إلتقاطها.")
+            }
+        }.sheet(isPresented: $openCameraRoll){
+            ImagePicker(selectedImage: $imageSelected,
+             sourceType: .camera)
+        }
+    }
+}
+struct MainPage_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPage()
+    }
+}
